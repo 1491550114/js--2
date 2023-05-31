332fa3bb6bb4ef26be8b8cb8613fdf0edc9891a1
1491550114 <1491550114@qq.com>
2023-05-31 17:23:26 +0800

    1 file modified

--- index.js
+++ index.js
@@ -349,3 +349,43 @@ telephoneCheck('1 555-555-5555');
 telephoneCheck('1(555)555-5555');
 telephoneCheck('1 555)555-5555');
 telephoneCheck('5555555555');
+
+/**12.计算找零 */
+function checkCashRegister(price, cash, cid) {
+  let change = Math.round((cash - price) * 100);
+  let currency = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000];
+  let arrCid = [],
+    sum = 0;
+
+  let sumCid = function () {
+    let cidMap = cid.map((val) => Math.round(val[1] * 100));
+    for (let i = 0; i < cidMap.length; i++) {
+      sum += cidMap[i];
+    }
+    return sum;
+  };
+  let res = sumCid();
+  console.log('taotao');
+  if (res === change) {
+    return { status: 'CLOSED', change: cid };
+  } else if (res < change) {
+    return { status: 'INSUFFICIENT_FUNDS', change: [] };
+  } else {
+    for (let i = currency.length - 1; i > -1; i--) {
+      while (change >= currency[i]) {}
+    }
+    console.log('333333333');
+  }
+}
+
+checkCashRegister(19.5, 20, [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100],
+]);
